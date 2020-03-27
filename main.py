a8cc6df1cebe6f71de5658bafeab949c1882d81b
Greccu <56735903+Greccu@users.noreply.github.com>
2020-03-27 19:36:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,150 +5,11 @@ from random import *
 from copy import copy
 from time import time
 from math import log,sqrt
+from sortari import *
 
 
 
-############################# sortari ########################
-
-
-def bubblesort(l):
-    if(len(l)>=30000):
-        return "Algoritm inoptim, nu se va executa sortarea"
-    k=copy(l)
-    ok=1
-    for i in range(1,len(k)-1):
-        ok=0
-        for j in range(len(k)-i):           
-            if(k[j]>k[j+1]):
-                ok=1
-                k[j],k[j+1]=k[j+1],k[j]
-        if ok==0:
-            break
-    return k
-
-def cmmdc(l):
-    i=0
-    while l[i]==0:
-        i-=-1
-    d=l[i]
-    for i in range(1,len(l)):
-        c=l[i]
-        if c == 0:
-            break
-        while c != 0: 
-            if c>=d:
-                c-=d
-            else:
-                d-=c
-    return d
-
-def countsort(l):
-    if len(l)==0:
-        return l
-    d=cmmdc(l)
-    m=min(l)    # daca numerele nu pot fi aduse intr-un interval
-    M=max(l)    # mai mic decat 1mil, nu se vor sorta elementele 
-    if M//d-m//d>=100000:
-        return "Algoritm inoptim, nu se va executa sortarea"
-    m//=d
-    k=copy(l)
-    p=0     #pentru a sti la final daca elementele au fost prelucrate
-    if M//d-m//d<1000:     # in acest caz optimizarea consuma timp inutil(cred)
-        p=1                     #
-        for i in range(len(k)):     # prelucrare lista
-            k[i]=k[i]//d-m
-        M-=m    # maximul listei prelucrate
-        
-    f=[0]*(M+1)
-    for i in k:
-        f[i]+=1
-    j=0
-    for i in range(M+1):
-        while f[i]>0:
-            k[j]=i
-            j+=1 
-            f[i]-=1
-    if p==1:
-        for i in range(len(k)):
-            k[i]=(k[i]+m)*d
-    return k
-
-def convert(k):
-    K=[]
-    for values in k.values():
-        for value in values:
-            K.append(value)
-    return K
-    
-
-def radixsort(l):
-    L=copy(l)
-    baza_radix=256
-    curent=1
-    k={}
-    n=(int(log(max(l),baza_radix)) + 1)
-    for _ in range(n):
-        for i in range(baza_radix):
-            k[i]=[]
-        for i in L:
-            k[(i//curent)%baza_radix].append(i)
-        L=convert(k)
-        curent*=baza_radix
-    return L
-
-def pivot(l):                           #mediana a 3 numere random din lista
-    a,b,c=choice(l),choice(l),choice(l)
-    if a>b:
-	    if a < c:
-	        return a
-	    elif b > c:
-	        return b
-	    else:
-	        return c
-    else:
-	    if a > c:
-	        return a
-	    elif b < c:
-	        return b
-	    else:
-	        return c
-
-
-def quicksort(l):
-    p=pivot(l)
-    low=[]
-    high=[]
-    return l
-    
-def merge(l1,l2):
-    i=0
-    j=0
-    l3=[]
-    while i<len(l1) and j<len(l2):
-        if l1[i]<=l2[j]:
-            l3.append(l1[i])
-            i+=1
-        else:
-            l3.append(l2[j])
-            j+=1
-    while i < len(l1): 
-            l3.append(l1[i])
-            i+=1
-    while j < len(l2): 
-            l3.append(l2[j])
-            j+=1
-    return l3
-    
-def mergesort(l):
-    if len(l)<=1:
-        return l
-    m=len(l)//2
-    l1=l[:m]
-    l2=l[m:]
-    k1=mergesort(l1)
-    k2=mergesort(l2)
-    k=merge(k1,k2)
-    return k
+########################## generator de numere ################
     
 
 def generator(nr,maxim=1000000):
@@ -164,14 +25,13 @@ def generator(nr,maxim=1000000):
 
 def timp():
     t = time()
-    format(float(t), '.2f')
-    t = float(t)
+    t = round(t,2)
     return t
 
 def Test(func,l):
     t = timp()
     m = func(l)
-    t = timp()-t
+    t = round(timp()-t,2)
     if isinstance(m, str):
         return -1, -1
     if m==sorted(m):
@@ -179,75 +39,38 @@ def Test(func,l):
     else:
         return 0, t
 
-############################# teste ##########################
-
 ##  -1 = Sortare inoptima
 ##  0 = Sortare incorecta
 ##  1 = Sortare corecta
 
-def Test1_lista_data():
-    global f
-    l = f.readline().split()
-    l = [int(i) for i in l]
-    return l
+############################## worst time ####################
 
-def Test2_lista_generata():
-    global f
-    z = f.readline()
-    x, y = z.split()
-    x, y = int(x), int(y)
-    print("Generare numere...")
-    print()
-    l = generator(x, y)
-    return l
+def best(l):
+    m=-1
+    for i in l:
+        if m==-1:
+            m=i
+        elif i<=m and i!=-1:
+            m=i
+    return m
 
-def Test3_lista_crescatoare():
-    maxim = 10000
-    l = [i for i in range(1,maxim+1)]
-    return l
-
-def Test4_lista_descrescatoare():
-    maxim = 10000
-    l = [maxim-i for i in range(maxim)]
-    return l
-
-def Test5_same_number():
-    nr = 1
-    maxim = 10000
-    l = [nr for _ in range(maxim)]
-    return l
-
-def Test6_lista_vida():
-    l = []
-    return l
-
-def Test7_putine_nr_mari():
-    nr = 500
-    max = 100000
-    l = generator(nr,max)
-    return l
-
-def Test8_multe_nr_mici():
-    nr = 100000
-    max = 100
-    l = generator(nr,max)
-    return l
 
 ############################# main ###########################
 
 f=open("input.txt")
 sortari=[bubblesort,countsort,radixsort,quicksort,mergesort]
-teste=[Test1_lista_data, Test2_lista_generata, Test3_lista_crescatoare, Test4_lista_descrescatoare, Test5_same_number, Test6_lista_vida, Test7_putine_nr_mari, Test8_multe_nr_mici]
+f.readline()
+n=int(f.readline())
 timpi=[]
-for i in range(len(teste)):
-    timpi.append([0]*len(sortari))
-    
+for i in range(n):
+    timpi.append([0]*5)
 
-i=0
-for test in teste:
-    print(test.__name__,end="\n-----------------------------------------------\n")
+for i in range(n):
     j=0
-    l = test()
+    m,M=f.readline().split()
+    m,M=int(m),int(M)
+    print("Testul numarul {} : {} numere cu maximul {}".format(i+1,m,M),end="\n-----------------------------------------------\n")
+    l = generator(m,M)
     for sr in sortari:
         print(sr.__name__)
         v,t = Test(sr, l)
@@ -261,7 +84,9 @@ for test in teste:
             print("Sortare incorecta")
         print()
         j+=1
-    i+=1
+    w=max(timpi[i])
+    b=best(timpi[i])
+    print("Cel mai bun timp {}, cel mai slab timp {}\n".format(b,w))
 print(timpi)
 
 f.close()
---
+++ sortari.py
@@ -0,0 +1,172 @@
+from copy import copy
+from math import log,sqrt
+from random import choice
+
+def bubblesort(l):
+    if (len(l) >= 30000):
+        return "Algoritm inoptim, nu se va executa sortarea"
+    k = copy(l)
+    ok = 1
+    for i in range(1, len(k) - 1):
+        ok = 0
+        for j in range(len(k) - i):
+            if (k[j] > k[j + 1]):
+                ok = 1
+                k[j], k[j + 1] = k[j + 1], k[j]
+        if ok == 0:
+            break
+    return k
+
+
+def cmmdc(l):
+    if len(l)==0:
+        return 0
+    i = 0
+    while l[i] == 0 and i<len(l):
+        i -= -1
+    d = l[i]
+    for i in range(1, len(l)):
+        c = l[i]
+        if c == 0:
+            break
+        while c != 0:
+            if c >= d:
+                c -= d
+            else:
+                d -= c
+    return d
+
+
+def countsort(l):
+    if len(l) == 0:
+        return l
+    d = cmmdc(l)
+    m = min(l)  # daca numerele nu pot fi aduse intr-un interval
+    M = max(l)  # mai mic decat 1mil, nu se vor sorta elementele
+    if M // d - m // d >= 100000:
+        return "Algoritm inoptim, nu se va executa sortarea"
+    m //= d
+    k = copy(l)
+    p = 0  # pentru a sti la final daca elementele au fost prelucrate
+    if M // d - m // d < 1000:  # in acest caz optimizarea consuma timp inutil(cred)
+        p = 1  #
+        for i in range(len(k)):  # prelucrare lista
+            k[i] = k[i] // d - m
+        M -= m  # maximul listei prelucrate
+
+    f = [0] * (M + 1)
+    for i in k:
+        f[i] += 1
+    j = 0
+    for i in range(M + 1):
+        while f[i] > 0:
+            k[j] = i
+            j += 1
+            f[i] -= 1
+    if p == 1:
+        for i in range(len(k)):
+            k[i] = (k[i] + m) * d
+    return k
+
+
+def convert(k):
+    K = []
+    for values in k.values():
+        for value in values:
+            K.append(value)
+    return K
+
+
+def radixsort(l):
+    L = copy(l)
+    if len(L)==0:
+        return L
+    baza_radix = 256
+    curent = 1
+    k = {}
+    n = (int(log(max(l), baza_radix)) + 1)
+    for _ in range(n):
+        for i in range(baza_radix):
+            k[i] = []
+        for i in L:
+            k[(i // curent) % baza_radix].append(i)
+        L = convert(k)
+        curent *= baza_radix
+    return L
+
+
+def pivot(a,b,c):  # mediana a 3 numere random din lista
+    if a > b:
+        if a < c:
+            return a
+        elif b > c:
+            return b
+        else:
+            return c
+    else:
+        if a > c:
+            return a
+        elif b < c:
+            return b
+        else:
+            return c
+
+def partition(l,st, dr):
+    p = pivot(choice(l[st:dr + 1]), choice(l[st:dr + 1]), choice(l[st:dr + 1]))
+    i = st - 1
+    j = dr + 1
+    while 1:
+        i += 1
+        while l[i] < p:
+            i += 1
+        j -= 1
+        while l[j] > p:
+            j -= 1
+        if i < j:
+            l[i], l[j] = l[j], l[i]
+        else:
+            return j
+
+def qs(l,st,dr):
+    if st < dr:
+        p = partition(l,st, dr)
+        qs(l, st, p)
+        qs(l, p + 1, dr)
+
+
+def quicksort(l):
+    L=copy(l)
+    qs(L,0,int(len(L)-1))
+    return L
+
+
+def merge(l1, l2):
+    i = 0
+    j = 0
+    l3 = []
+    while i < len(l1) and j < len(l2):
+        if l1[i] <= l2[j]:
+            l3.append(l1[i])
+            i += 1
+        else:
+            l3.append(l2[j])
+            j += 1
+    while i < len(l1):
+        l3.append(l1[i])
+        i += 1
+    while j < len(l2):
+        l3.append(l2[j])
+        j += 1
+    return l3
+
+
+def mergesort(l):
+    if len(l) <= 1:
+        return l
+    m = len(l) // 2
+    l1 = l[:m]
+    l2 = l[m:]
+    k1 = mergesort(l1)
+    k2 = mergesort(l2)
+    k = merge(k1, k2)
+    return k
